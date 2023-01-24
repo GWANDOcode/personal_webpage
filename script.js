1fdc6eae06b0762b85e3c3a626528b13a9878341
GWANDO.code <118937578+GWANDOcode@users.noreply.github.com>
2023-01-24 20:05:47 +0100

    Add togglable Contact info

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 let viewportWidth = window.innerWidth;
 let viewportHeight = window.innerHeight;
 
+let contactStatus = false;
+
 const footer = document.querySelector("footer");
+const contactArrow = document.querySelector("#contact_arrow");
+
+const contactInfo = document.querySelector(".contact_info");
+
+contactArrow.addEventListener("click", () => {
+    contactInfo.classList.toggle("hide");
+    contactStatus = true;
+});
