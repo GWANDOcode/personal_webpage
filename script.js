d674853b5619b2da35d7bad19d3af6d55c5c20ce
GWANDO.code <118937578+GWANDOcode@users.noreply.github.com>
2023-01-24 23:59:40 +0100

    Add boilerplate layout to webpages

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 let viewportWidth = window.innerWidth;
 let viewportHeight = window.innerHeight;
 
-let contactStatus = false;
-
+const container = document.querySelector(".container");
 const footer = document.querySelector("footer");
 const contactArrow = document.querySelector("#contact_arrow");
 
 const contactInfo = document.querySelector(".contact_info");
 
 contactArrow.addEventListener("click", () => {
-    contactInfo.classList.toggle("hide");
-    contactStatus = true;
+        contactInfo.classList.toggle("hide");
+        contactInfo.scrollIntoView();
 });
