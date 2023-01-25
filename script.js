208ce6077844da977a1f98d7be4d196ce4970628
GWANDO.code <118937578+GWANDOcode@users.noreply.github.com>
2023-01-25 02:23:09 +0100

    Add color change on magazin hover

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let viewportWidth = window.innerWidth;
 let viewportHeight = window.innerHeight;
 
+const body = document.querySelector("body");
 const container = document.querySelector(".container");
 
 const magazin = document.querySelector(".magazin_cover");
@@ -12,4 +13,13 @@ const contactInfo = document.querySelector(".contact_info");
 contactArrow.addEventListener("click", () => {
         contactInfo.classList.toggle("hide");
         contactInfo.scrollIntoView();
+});
+
+magazin.addEventListener("mouseover", () => {
+body.style.backgroundColor = "#FF5714";
+body.style.transition = ".5s";
+});
+
+magazin.addEventListener("mouseleave", () => {
+body.style.backgroundColor = "white";
 });
